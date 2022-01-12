021605554c40058b3f1313e3c0d0c5f77f1cf466
Matt Chapman <matt@mattchapman.io>
2022-01-13 09:37:15 +1300

    Add colorscheme

---
+++ lua/user/colorscheme.lua
@@ -0,0 +1,11 @@
+-- Set colorscheme
+local colorscheme = "tokyonight"
+
+-- Try set color scheme
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+
+-- Warn rather than breaking if color scheme not found
+if not status_ok then
+  vim.notify("colorscheme " .. colorscheme .. " not found!")
+  return
+end
