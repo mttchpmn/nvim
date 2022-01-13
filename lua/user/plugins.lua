fbd1666fff8c7b6a6c4b202c3be1a0d7eed4466b
Matt Chapman <matt@mattchapman.io>
2022-01-13 22:52:58 +1300

    Add Treesitter

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -44,8 +44,8 @@ return packer.startup(function(use)
   -- My plugins here
   use "wbthomason/packer.nvim" -- Have packer manage itself
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
-  use "nvim-lua/plenary.nvim" -- Useful lua functions used in lots of plugins
-  
+  use "nvim-lua/plenary.nvim" -- Useful lua functions used in lots of plugin
+
   -- Color
   use "folke/tokyonight.nvim"
 
@@ -68,6 +68,13 @@ return packer.startup(function(use)
 
   -- Telescope
   use "nvim-telescope/telescope.nvim"
+
+  -- TreeSitter
+  use {
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate",
+  }
+  use "p00f/nvim-ts-rainbow"
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
