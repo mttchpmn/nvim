59559f31d6435b933f1fc6caef4a2c6f19782bac
Matt Chapman <matt@mattchapman.io>
2022-01-13 18:05:51 +1300

    Add Telescope

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -68,3 +68,10 @@ set_keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
 set_keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
 set_keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 set_keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts) 
+
+-- Telescope --
+-- TODO - git branches and git status
+set_keymap("n", "<leader>f", "<cmd>Telescope find_files<cr>", opts)
+-- set_keymap("n", "<leader>f", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
+set_keymap("n", "<c-t>", "<cmd>Telescope live_grep<cr>", opts)
+
