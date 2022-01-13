63e9c17fe93d55e22eb9d87d68b5e24651fc052e
Matt Chapman <matt@mattchapman.io>
2022-01-14 09:43:10 +1300

    Add node_modules to telescope ignore

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -11,6 +11,8 @@ telescope.setup {
     selection_caret = " ",
     path_display = { "smart" },
 
+    file_ignore_patterns = {"node_modules"},
+
     mappings = {
       i = {
         ["<C-n>"] = actions.cycle_history_next,
