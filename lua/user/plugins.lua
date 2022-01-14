d37fcd2b8c8ecc7621a134fa122fe644d5b19ad4
Matt Chapman <matt@mattchapman.io>
2022-01-14 20:44:57 +1300

    Add toggleterm

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -60,6 +60,7 @@ return packer.startup(function(use)
   use "saadparwaiz1/cmp_luasnip" -- snippet completions
   use "hrsh7th/cmp-nvim-lsp"
   use "hrsh7th/cmp-nvim-lua"
+  use "akinsho/toggleterm.nvim"
 
   -- Snippets
   use "L3MON4D3/LuaSnip" --snippet engine
