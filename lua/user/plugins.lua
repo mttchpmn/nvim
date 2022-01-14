0f389198018cafcf1cd9e5e4c26548b09ff8a026
Matt Chapman <matt@mattchapman.io>
2022-01-14 13:47:59 +1300

    Add Gitsigns

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -80,6 +80,8 @@ return packer.startup(function(use)
   use "p00f/nvim-ts-rainbow"
   use "JoosepAlviste/nvim-ts-context-commentstring"
 
+  -- Git
+  use "lewis6991/gitsigns.nvim"
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
